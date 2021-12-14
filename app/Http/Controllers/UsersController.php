411ccbaf669ff3cc0e05192dc2bd5f7a5c068153
sinmu <3038267725@qq.com>
2021-12-14 17:57:02 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,17 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','create','store']
+        ]);
+
+        // 只让未登录用户访问注册页面：
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
     public function create(){
         // view('users.create') 和 view('users/create') 的区别不大，建议写 view('users.create')
         return view('users.create');
@@ -35,10 +46,12 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user, Request $request){
+        $this->authorize('update',$user);
         $this->validate($request,[
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
