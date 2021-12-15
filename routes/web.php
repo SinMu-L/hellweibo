fdb141325592aef8aaf93321ac82702e760cb836
sinmu <3038267725@qq.com>
2021-12-15 18:30:03 +0800

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,5 @@ Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('p
 
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
+
+Route::resource('statuses',StatusesController::class,['only'=>['store','destroy']]);
