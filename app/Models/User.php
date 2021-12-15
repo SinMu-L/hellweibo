2d39e02721a0af87a5869e9b98343590fa98cd40
sinmu <3038267725@qq.com>
2021-12-15 19:49:55 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,36 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+
+    // 获取当前用户的粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 获取当前用户关注的人
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids){
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids){
+        if( !is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFolllowing($user_id){
+        return $this->followings->contains($user_id);
+    }
 }
